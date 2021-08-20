e3950e1e16676043e96df830d10b7ce2d9f8c326
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-08-20 05:18:51 +0400

    Update files

---
+++ app/src/views/service.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="service">
+        Service
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'service'
+}
+</script>
+
+<style lang="scss">
+
+.service {
+    font-size: 20px;
+    font-weight: bold;
+}
+
+</style>
